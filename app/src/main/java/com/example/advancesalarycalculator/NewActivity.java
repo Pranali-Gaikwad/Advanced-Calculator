6fa79506b2e69aec892f64d6fe814f06656bbc07
Pranali <gaikwadpranali1995@gmail.com>
2020-06-23 02:34:32 +0530

    Change UI margins.

--- app/src/main/java/com/example/advancesalarycalculator/NewActivity.java
+++ app/src/main/java/com/example/advancesalarycalculator/NewActivity.java
@@ -3,7 +3,6 @@ package com.example.advancesalarycalculator;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
